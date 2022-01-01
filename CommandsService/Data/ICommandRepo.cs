e30f37202991573cff02d0bd4bb9fd80a62f79c5
Koray Aksoy <burakkorayaksoy@hotmail.com>
2022-01-01 21:03:07 +0300

    EventProcessor added and dtos created for RabbitMQ

--- CommandsService/Data/ICommandRepo.cs
+++ CommandsService/Data/ICommandRepo.cs
@@ -9,6 +9,7 @@ namespace CommandsService.Data
         IEnumerable<Platform> GetAllPlatforms();
         void CreatePlatform(Platform plat);
         bool PlatformExist(int platformId);
+        bool ExternalPlatformExist(int externalPlatformId);
         IEnumerable<Command> GetCommandsForPlatform(int platformId);
         Command GetCommand(int platformId, int commandId);
         void CreateCommand(int platformId, Command command);
